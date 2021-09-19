ff777e5b04ca385f60dbfd575fb5e059eab547dc
headzik <lukaszstrzelecki9@gmail.com>
2021-09-19 07:21:48 +0200

    Upload project

--- src/main/java/at/fhooe/cc/ecarrenting/CarController.java
+++ src/main/java/at/fhooe/cc/ecarrenting/CarController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/cars")
